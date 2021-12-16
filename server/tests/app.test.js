bdeb86f082eb37cf21b6a731c9c1b5ac4bd326eb
Kevin Farias <kevin.farias.dev@gmail.com>
2021-12-16 16:57:26 -0300

    adding one test

--- server/tests/app.test.js
+++ server/tests/app.test.js
@@ -1,4 +1,23 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -14,6 +33,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const index_1 = __importDefault(require("../index"));
 const supertest_1 = __importDefault(require("supertest"));
+const imageController_1 = __importDefault(require("../controllers/imageController"));
+const fs_1 = __importStar(require("fs"));
 describe('Test app main routes', () => {
     it('GET /convert/fjord.jpg = HAVE TO CONVERT THE IMAGE SUCCESSFULLY', () => __awaiter(void 0, void 0, void 0, function* () {
         yield (0, supertest_1.default)(index_1.default).get('/convert/fjord.jpg').expect(200);
@@ -22,3 +43,17 @@ describe('Test app main routes', () => {
         yield (0, supertest_1.default)(index_1.default).get('/convert/fjord1.jpg').expect(500);
     }));
 });
+describe('Unit testing', () => __awaiter(void 0, void 0, void 0, function* () {
+    const dirName = __dirname + '/../../images/converted';
+    const fileName = 'fjord.jpg';
+    beforeAll(() => __awaiter(void 0, void 0, void 0, function* () {
+        if (fs_1.default.existsSync(dirName + '/' + fileName)) {
+            yield fs_1.promises.unlink(dirName + '/' + fileName);
+        }
+    }));
+    it('Test creating image', () => __awaiter(void 0, void 0, void 0, function* () {
+        yield imageController_1.default.convert(fileName);
+        const exists = fs_1.default.existsSync(dirName + '/' + fileName);
+        expect(exists).toBeTruthy();
+    }));
+}));
--- src/tests/app.test.ts
+++ src/tests/app.test.ts
@@ -1,5 +1,7 @@
 import app from '../index';
 import supertest from 'supertest';
+import imageController from '../controllers/imageController';
+import fs, { promises as fsPromises } from 'fs';
 
 describe('Test app main routes', (): void => {
   it('GET /convert/fjord.jpg = HAVE TO CONVERT THE IMAGE SUCCESSFULLY', async (): Promise<void> => {
@@ -8,5 +10,20 @@ describe('Test app main routes', (): void => {
 
   it('GET /convert/fjord1.jpg = HAVE NOT TO CONVERT THE IMAGE', async (): Promise<void> => {
     await supertest(app).get('/convert/fjord1.jpg').expect(500);
+  });
+});
+
+describe('Unit testing', async (): Promise<void> => {
+  const dirName = __dirname + '/../../images/converted';
+  const fileName = 'fjord.jpg';
+  beforeAll(async () => {
+    if (fs.existsSync(dirName + '/' + fileName)) {
+      await fsPromises.unlink(dirName + '/' + fileName);
+    }
+  });
+  it('Test creating image', async () => {
+    await imageController.convert(fileName);
+    const exists = fs.existsSync(dirName + '/' + fileName);
+    expect(exists).toBeTruthy();
   });
 });
